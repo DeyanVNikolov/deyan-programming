88268b911d4d79ada7bd7d0859dad08dc4db0a04
Deyan Nikolov <didonikolovbg@gmail.com>
2022-01-29 15:45:49 +0200

    For Cycle

---
+++ 7 - for_cycle_1/9.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+left_sum = 0
+right_sum = 0
+
+for i in range(1, n + 1):
+    num = int(input())
+    left_sum += num
+
+for i in range(1, n + 1):
+    num = int(input())
+    right_sum += num
+
+if left_sum == right_sum:
+    print(f"Yes, sum = {right_sum}")
+else:
+    diff = abs(left_sum - right_sum)
+    print(f"No, diff = {diff}")
